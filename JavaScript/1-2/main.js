70eec7ccc010c711962cb9183878a5246deab2b5
elan0 <gasmaskrobotomi@gmail.com>
2021-06-23 15:14:36 +0900

    1-2

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,21 @@
+let name = "hoge";
+console.log(name);
+name = "fuga";
+console.log(name);
+
+let name2 = "池田";
+console.log(name2 + "君");
+let number = 10;
+console.log(number * 3);
+console.log( `${name2}君は${number}月生まれです`);
+
+
+/*
+課題、エラーを修正してください
+*/
+//エラー
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
