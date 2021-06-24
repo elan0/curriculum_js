d8a39f8dec4fe6c3680cba21bcb5f7eed6ab59a2
elan0 <gasmaskrobotomi@gmail.com>
2021-06-24 15:03:17 +0900

    1-7

--- JavaScript/1-7/main.js
+++ JavaScript/1-7/main.js
@@ -1,7 +1,24 @@
 //Humanクラスを作成
 class Human {
     constructor(name, age) {
-
+        this.name = name;
+        this.age = age;
     }
 }
-let yamada = new Human("yamada", 25);
+let yamada = new Human("山田", 25);
+console.log(yamada.name);
+
+class Taiyaki {
+    constructor(cream) {
+        this.cream = cream;
+    }
+}
+
+let taiyaki1 = new Taiyaki("あんこ");
+console.log("中身は"+taiyaki1.cream+"です");
+
+let taiyaki2 = new Taiyaki("クリーム");
+console.log("中身は"+taiyaki2.cream+"です");
+
+let taiyaki3 = new Taiyaki("チーズ");
+console.log("中身は"+taiyaki3.cream+"です");
