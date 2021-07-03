e4058af0dcf5883e6c14983859dc9c173a0c0772
elan0 <gasmaskrobotomi@gmail.com>
2021-07-03 15:46:54 +0900

    2-3

---
+++ JavaScript/2-3/main.js
@@ -0,0 +1,35 @@
+/*
+let btn = document.getElementById('btn');
+btn.onclick = function() {
+    alert('ボタンがクリックされました');
+}
+*/
+
+/*
+onclick     :クリックされた時
+ondblclick  :ダブルクリックされた時
+onmouseover :マウスが乗った時
+onmouseout  :マウスが外れた時
+onmousedown :マウスのボタンを押したとき
+onmouseup   :マウスのボタンを離したとき
+onmousemove :マウスを動かしたとき
+onfocus     :フォーカスが当たった時
+onblur      :フォーカスが外れた時
+onchange    :formの値が変わった時
+onsubmit    :formを送信する時
+*/
+
+/*
+//複数の動作
+let box = document.getElementById('box');
+let becorn = document.getElementById('becorn');
+//ボタンがクリックされたら
+becorn.addEventListener('click', function() {
+    box.style.backgroundColor = 'red';
+}, false);
+//ボタンが押されたら、アラートを出す
+becorn.addEventListener('click', function() {
+    alert('ボタンが押されました');
+}, false);
+*/
+
