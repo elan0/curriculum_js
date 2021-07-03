09c9a4c994d01969dd78bdff693de104aea5fe53
elan0 <gasmaskrobotomi@gmail.com>
2021-07-03 22:44:00 +0900

    2-4

---
+++ JavaScript/2-4/main.js
@@ -0,0 +1,11 @@
+/*
+document.querySelector('#createBtn').addEventListener('click', function() {
+    //<il></il>が入る
+    let item = document.createElement('il');
+    //
+    item.textContent = 'item';
+
+    let ul = document.querySelector('ul');
+    ul.appendChild(item);
+})
+*/
